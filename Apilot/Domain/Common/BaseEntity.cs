5d3736a0b4dc042a47a7304ad70bdaa8838e0b21
youssef-khalifi <youssefkhalifi767@gmail.com>
2025-04-23 11:53:19 +0200

    Initial Project

---
+++ Apilot/Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apilot.Domain.Common;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public bool IsSync { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? LastSyncDate { get; set; }
+    public Guid SyncId { get; set; }
+    [Required]
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
+}
